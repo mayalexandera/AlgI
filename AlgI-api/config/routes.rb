2bde22acad1a5b39e1938af1268744be017e597e
mayalexandera <maya.blevins@gmail.com>
2020-07-26 21:11:05 -0400

    add api/v1 directory

--- AlgI-api/config/routes.rb
+++ AlgI-api/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'image_properties/upvote:integer'
+  get 'image_properties/downvote:integer'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
