ef5dd32f3356dfd076258e9903ac89e90d5c2be6
mayalexandera <maya.blevins@gmail.com>
2020-07-27 17:05:48 -0400

    translated BFS

--- algi-frontend/src/scripts/BFS.js
+++ algi-frontend/src/scripts/BFS.js
@@ -1,8 +1,28 @@
-import Canvas from './Canvas';
+import Canvas from "./Canvas";
 
 class BFS {
-    constructor() {
-    }
-}
+  constructor(val) {
+    this.val = val; //start position
+    this.neighbors = [];
+  }
+  //addNeighbor(node) {
+  //  this.neighbors.push(node);
 
+  static bfs(startPos, targetVal) {
+    let queue = [startPos];
+    visited = new Set();
+
+    while (queue.length > 0) {
+      node = queue.shift();
+      while (!visited.has(node)) {
+        if (node.val === targetVal) {
+          return node.val;
+        }
+        visited.add(node);
+        queue.push(node.neighbors);
+      }
+    }
+    return nil;
+  }
+}
 export default BFS;
