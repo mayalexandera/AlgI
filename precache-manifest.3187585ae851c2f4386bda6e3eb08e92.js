beeb87a8385c1b8e4e8a75a780daa03999674fa2
mayalexandera <maya.blevins@gmail.com>
2020-08-03 19:23:15 -0400

    Updates

---
+++ precache-manifest.3187585ae851c2f4386bda6e3eb08e92.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549ff07e35b6820896ad2daf88152152",
+    "url": "/AlgI/index.html"
+  },
+  {
+    "revision": "a16bc5c56af2edb9eefa",
+    "url": "/AlgI/static/css/main.0b91ed50.chunk.css"
+  },
+  {
+    "revision": "bf19a8f28b0d76b67bdc",
+    "url": "/AlgI/static/js/2.878a54c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AlgI/static/js/2.878a54c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16bc5c56af2edb9eefa",
+    "url": "/AlgI/static/js/main.9cd3f5b1.chunk.js"
+  },
+  {
+    "revision": "4572b46b5d614edcd2d2",
+    "url": "/AlgI/static/js/runtime-main.814a667e.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/AlgI/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "edae1f8bd346337b683e2822c5615ab4",
+    "url": "/AlgI/static/media/bullseye.edae1f8b.png"
+  },
+  {
+    "revision": "879b9b2985999a81c1195cd6818b77da",
+    "url": "/AlgI/static/media/picaso-01.879b9b29.jpg"
+  },
+  {
+    "revision": "ea73172acb51d412b24f79da2e1f077f",
+    "url": "/AlgI/static/media/picaso-02.ea73172a.jpg"
+  },
+  {
+    "revision": "d474668dcb9768cf85375e50c5c58b30",
+    "url": "/AlgI/static/media/picaso-03.d474668d.jpg"
+  },
+  {
+    "revision": "8d042f939d50bca61afd1a48989941cc",
+    "url": "/AlgI/static/media/picaso-04.8d042f93.jpg"
+  },
+  {
+    "revision": "bcdd4e658b32d4cef0f577eeb05587d2",
+    "url": "/AlgI/static/media/vincent-01.bcdd4e65.jpg"
+  },
+  {
+    "revision": "a5d341c704f42ba45eb8376e73abf014",
+    "url": "/AlgI/static/media/vincent-02.a5d341c7.jpg"
+  },
+  {
+    "revision": "8461ed9547c54470e8c67be5184aaca5",
+    "url": "/AlgI/static/media/vincent-03.8461ed95.jpg"
+  },
+  {
+    "revision": "c8ba263b99daf9ec2dd9b6e38f719cf1",
+    "url": "/AlgI/static/media/vincent-04.c8ba263b.jpg"
+  },
+  {
+    "revision": "c25e623eec7a9ad1dd9f5005f4b6ff38",
+    "url": "/AlgI/static/media/vincent.c25e623e.jpg"
+  }
+]);
